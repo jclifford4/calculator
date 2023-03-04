c16d3c340b34e6c339ef35784d65f01f8c298242
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-03-03 20:36:40 -0800

    Add math functions

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add(first, second)
+{
+    return first + second;
+}
+
+function subtract(first, second)
+{
+    return first - second;
+}
+
+function multiply(first, second)
+{
+    return first * second;
+}
+
+function divide(first, second)
+{
+    return first / second;
+}
+
+function operate(operator, first, second)
+{
+    if (operator === '+')
+        return add(first, second);
+
+    if (operator === '-')
+        return subtract(first, second);
+
+    if (operator === '*')
+        return multiply(first, second);
+
+    if (operator === '/')
+        return divide(first, second);
+    
+}
