997a0ec2c442f412f459a17e7f4a75ad166d5b88
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2023-03-06 15:30:25 -0800

    Complete essential requirements: display, methods, clear, float limit

--- calculator.js
+++ calculator.js
@@ -1,23 +1,43 @@
+// Query the main componenets
+const numbersDisplay = document.querySelector('.numbers');
+
+const buttons = document.querySelectorAll('.button');
+
+
+
+// Data
+let operator, displayValue = '';
+let numbers = [];
+let operators = [];
+let currentNum;
+let currentNumString = '';
+
+
+// Add method
 function add(first, second)
 {
     return first + second;
 }
 
+// Subtract method
 function subtract(first, second)
 {
     return first - second;
 }
 
+// Multiply method
 function multiply(first, second)
 {
     return first * second;
 }
 
+// Divide method
 function divide(first, second)
 {
     return first / second;
 }
 
+// Calculate result based on two numbers and an operator
 function operate(operator, first, second)
 {
     if (operator === '+')
@@ -34,139 +54,71 @@ function operate(operator, first, second)
     
 }
 
-const numbersDisplay = document.querySelector('.numbers');
-console.log(numbersDisplay);
-
-const buttons = document.querySelectorAll('.button');
-console.log(buttons);
-
-let operator, displayValue = '';
-let numbers = [];
-let operators = [];
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        
-        // Append number to display
-        if (button.classList.contains('number'))
-        {
-            addNumberToDisplay(button);
-            numbersDisplay.innerHTML = displayValue;
-        }
-
-        // Clear display and stored data
-        if (button.classList.contains('clear'))
-        {
-            clearDisplay();
-            clearContainers();
-            numbersDisplay.innerHTML = '0';
-        }
-
-        if (displayValue != '')
-        {
-
-            if (button.classList.contains('add'))
-            {
-                operator = '+';
-                appendNumber(Number(displayValue));
-                appendOperator(operator);
-                addOperatorToDisplay(operator);
-                clearDisplay();
-                console.log(numbers);
-                console.log(operators); 
-                
-            }
-            
-            if (button.classList.contains('subtract'))
-            {
-                operator = '-';
-                appendNumber(Number(displayValue));
-                appendOperator(operator);
-                addOperatorToDisplay(operator);
-                clearDisplay();
-                console.log(numbers);
-                console.log(operators); 
-            }
-    
-            if (button.classList.contains('multiply'))
-            {
-                operator = '*';
-                appendNumber(Number(displayValue));
-                appendOperator(operator);
-                addOperatorToDisplay(operator);
-                clearDisplay();
-                console.log(numbers);
-                console.log(operators); 
-            }
-    
-            if (button.classList.contains('divide'))
-            {
-                operator = '/';
-                appendNumber(Number(displayValue));
-                appendOperator(operator);
-                addOperatorToDisplay(operator);
-                clearDisplay();
-                console.log(numbers);
-                console.log(operators); 
-            }
-        }
-
-        if (button.classList.contains('enter'))
-        {
-            if (numbers.length < 1 && operators < 1)
-                numbersDisplay.innerHTML = 'ERROR';
-            else if (numbers.length >= 1)
-            {
-
-                operators.reverse();
-                numbers.push(Number(numbersDisplay.innerHTML));
-                numbers[0] = operate(operators[0], numbers[0], numbers.pop());
-                displayResult(numbers[0]);
-                displayValue = numbers[0];
-            }
-           
-        }
-
-    });
-   
-});
-
+// Display the calculation in the display
 function displayResult(value)
 {
-    clearDisplay();
     numbersDisplay.innerHTML += `${value}`;
 }
 
+// Append number to display
 function addNumberToDisplay(button)
 {
-    displayValue = displayValue + `${button.innerHTML}`;
-    console.log(displayValue);
+    numbersDisplay.innerHTML += `${button.innerHTML}`;
+    currentNumString += `${button.innerHTML}`;
+    displayValue = numbersDisplay.innerHTML;
 }
 
+// Append operator to display
 function addOperatorToDisplay(operator)
 {
-    numbersDisplay.innerHTML += ' ' + operator;
-    
+    numbersDisplay.innerHTML += ' ' + operator + ' ';
 }
 
+// Strings calculations together
+function addNumberToDisplaySequence(currNum)
+{
+    numbers[0] =  operate(operator, numbers[0], currNum);
+    numbersDisplay.innerHTML += `${currNum}`;
+    displayValue = numbers[0];
 
+    while(numbers.length >1 )
+        numbers.pop();
+    operators.pop();
+}
+
+function calculate(operator)
+{
+    appendNumber(Number(displayValue));
+    appendOperator(operator);
+    addOperatorToDisplay(operator);
+    if (numbers.length > 1)
+        numbers.pop();
+    currentNumString = '';
+}
+
+// Push to number container
 function appendNumber(number)
 {
     numbers.push(number);    
 }
 
+// Push to operator container
 function appendOperator(operator)
 {
     operators.push(operator);
 }
 
+// Wipe the display and data
 function clearDisplay()
 {
     numbersDisplay.innerHTML = '';  
     displayValue = '';
-    operator = undefined;
+    currentNum = undefined;
+    currentNumString = '';
+    
 }
 
+// Wipe the containers clean
 function clearContainers()
 {
     while(numbers.length !== 0)
@@ -176,14 +128,92 @@ function clearContainers()
         operators.pop();
 }
 
-function showAll()
-{
-    // numbersDisplay.innerHTML = `${first} ${operator} ${second} = `;
-    // console.log(`${numbersClicked[0]} ${numbersClicked[1]} ${numbersClicked[2]} = ${operate(numbersClicked[1], numbersClicked[0], numbersClicked[2])}`);
-    // numbersDisplay.innerHTML = (operate(numbersClicked[1], Number(numbersClicked[0]), Number(numbersClicked[2])));
-    // let result = numbersDisplay.innerHTML;
-    // console.log(typeof(result));
-}
+
+// Listener for clicks on buttons.
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        // Append number to display
+        if (button.classList.contains('number'))
+        {
+            if (numbersDisplay.innerHTML === "0")
+            {
+                clearDisplay();
+            }
+            
+            if (operator !== undefined)
+            {   
+                currentNum = Number(button.innerHTML);
+                currentNumString += `${currentNum}`;
+                numbersDisplay.innerHTML += `${currentNum}`;
+            }
+            else
+            {
+                addNumberToDisplay(button);
+            }
+        }
+
+        // Clear display and stored data
+        if (button.classList.contains('clear'))
+        {
+            clearDisplay();
+            clearContainers();
+            numbersDisplay.innerHTML = '';
+            operator = undefined;
+        }
+
+        // Math functions on click
+        if (displayValue != '')
+        {
+
+            if (button.classList.contains('add'))
+            {
+                operator = '+';
+                calculate(operator);
+            }
+            
+            if (button.classList.contains('subtract'))
+            {
+                operator = '-';
+                calculate(operator);  
+            }
+    
+            if (button.classList.contains('multiply'))
+            {
+                operator = '*';
+                calculate(operator);   
+            }
+    
+            if (button.classList.contains('divide'))
+            {
+                operator = '/';
+                calculate(operator);
+            }
+        }
+
+        // Result on click
+        if (button.classList.contains('enter'))
+        {
+            document.querySelector('.enter').classList.add('active');
+            if (numbers.length < 1 || numbers.length > 1 || currentNum === undefined)
+                numbersDisplay.innerHTML = 'ERROR';
+            else if (numbers.length >= 1)
+            {
+                appendNumber(Number(currentNumString));
+                addNumberToDisplaySequence(Number(currentNumString));
+                
+                displayValue = numbers[0];
+
+                if (displayValue % 1 !== 0)
+                    numbersDisplay.innerHTML = `${displayValue.toFixed(6)}`;
+                else
+                numbersDisplay.innerHTML = `${displayValue}`;
+            }
+        }
+    });
+});
+
+
 
 
 
